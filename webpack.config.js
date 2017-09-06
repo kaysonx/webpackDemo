e95380ce0cfbc98cdd0e5a569887e5256aa1d082
Qingsong Peng <qspeng@thoughtworks.com>
2017-09-06 11:31:35 +0800

    use CleanWebpackPlugin to clean the dist before build.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -36,7 +37,10 @@ module.exports = {
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        }),
+        new CleanWebpackPlugin(['dist']),
     ],
 };
 
